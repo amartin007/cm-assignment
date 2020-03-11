c6e1eeedc2da7b50f33ae248a224f8c1caf810aa
Anthony Martin <amartin007@gmail.com>
2020-03-10 22:25:26 -0500

    Initial layout of top view of component

--- src/components/PremiumRecipeCard.vue
+++ src/components/PremiumRecipeCard.vue
@@ -1,3 +1,69 @@
 <template>
-  <div>Premium Recipe Card Component Goes Here</div>
+  <div class="card-wrapper">
+    <div class="card-top">
+      <div class="card-img">
+        <img src="../assets/icons/heart_empty.svg" class="heart-icon" />
+        <div class="premium premium-bg"></div>
+        <div class="premium premium-content">
+          <img src="../assets/icons/trophy.svg" class="trophy-icon" />
+          <span> Premium Recipe</span>
+        </div>
+        <img src="../assets/overlay.png" />
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "PremiumRecipeCard",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped>
+.card-wrapper {
+  width: 343px;
+  margin: auto;
+  font-size: 14px;
+}
+.card-wrapper:hover {
+  cursor: pointer;
+}
+.card-wrapper:hover > .card-top {
+  opacity: 0.8;
+}
+.card-top {
+  position: relative;
+  height: 200px;
+  background: url("../assets/food.png");
+  border-radius: 12px 12px 0 0;
+}
+.heart-icon {
+  position: absolute;
+  top: 15px;
+  right: 15px;
+}
+.trophy-icon {
+  height: 14px;
+}
+.premium {
+  position: absolute;
+  bottom: 10px;
+  left: 10px;
+  width: 152px;
+}
+.premium-bg {
+  background-color: #eee;
+  opacity: 0.4;
+  height: 25px;
+  border-radius: 25px;
+  bottom: 9px;
+}
+.premium-content {
+  color: #fff;
+  font-weight: bold;
+}
+</style>
